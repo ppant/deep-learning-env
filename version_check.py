c5e08e675f341f55f59684d3034805344a4d695b
Pradeep K. Pant <pantpradeep@gmail.com>
2018-07-10 18:29:40 +0530

    Cheking versions of various MI libs

---
+++ version_check.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 10 18:00:24 2018
+
+@author: Pradeep
+"""
+# Check versions of module installed
+# scipy
+import scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+import pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+import statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+import sklearn
+print('sklearn: %s' % sklearn.__version__)
+
